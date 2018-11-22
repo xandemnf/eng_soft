b180b385422bfe1fb13853118f4fc60a7cfb0c62
Alexandre Gonçalves <xandemnf@gmail.com>
2018-11-22 06:24:30 -0200

    Versão Final para Entrega

--- twitter_clone/registra_usuariosql.sql
+++
@@ -1,6 +0,0 @@
-create table usuarios(
-	id int not null primary key auto_increment,
-	usuario varchar(50) not null,
-	email varchar(50) not null,
-	senha varchar(30) not null
-);
--- view.sql
+++
@@ -1,4 +0,0 @@
-create view maisVelhos as
-select codDoc, sexo, idade
-from docente
-order by idade desc;
